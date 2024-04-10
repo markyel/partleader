e57c49cfdf97239a6ae3268a50d5b117c1f86523
markyel <boag77@gmail.com>
2024-04-11 01:15:48 +0300

    init

---
+++ storage/framework/views/ec34d79765dc50af4a216cd4d46f2203.php
@@ -0,0 +1,19 @@
+<?php $__env->startSection('pagecontent'); ?>
+
+
+    <div class="" style="width: 100%; padding: 50px 150px; ">
+        <h4>Стать партнером </h4>
+        <p>
+            Оформите заявку на получение ценового предложения от ПартЛидер. От Вас больше ничего не требуется. Все остальные действия сделает ПартЛидер.
+        </p>
+        <p>
+            Для каждой позиции Вашей заявки будет найдено оптимальное ценовое предложение от одного из наших партнеров. Затем предложения по всем запчастям консолидируются и Вы получаете оптимальное по сроку и цене коммерческое предложение.
+        </p>
+    </div>
+
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/resources/views/partner.blade.php ENDPATH**/ ?>
