8f2b4a4f9f41a3155ac2f34410ca771ee09062d5
markyel <boag77@gmail.com>
2024-04-16 17:13:16 +0300

    init

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -136,15 +136,202 @@
         @yield('pagecontent')
 
 
+<style>
+    h6 {
+        font-family: sans-serif;
+        color: #fff;
+        font-size: 1.2rem;
+        padding-left: 1rem;
+
+    }
+    .h-40px {
+        height: 40px !important;
+    }
+    img, svg {
+        vertical-align: middle;
+    }
+
+    .nav-link-b {
+        display: block;
+        padding: var(--bs-nav-link-padding-y) var(--bs-nav-link-padding-x);
+        font-size: 0.938rem;;
+        font-weight: 500;
+        color: #c5c6cc;
+        background: none;
+        border: 0;
+        -webkit-transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+        transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out;
+        text-decoration: none;
+
+    }
+    .nav-link-b:hover{
+    text-decoration: none;
+        color: #e07979;
+    }
+</style>
 
 
 
+<footer class="bg-dark position-relative overflow-hidden pb-0 pt-6 pt-lg-8" data-bs-theme="dark" style="background: url(/images/botbg.svg) #163649 center  repeat-x ; ">
 
-<div class="row justify-content-md-center  m-0 p-0 w-100 bg-dark" style="background: url(/images/botbg.svg) #163649 center repeat-x ; width: 100%; height: 400px;" >
+
+
+    <div class="container position-relative mt-5">
+        <div class="row g-4 justify-content-between">
+
+            <!-- Widget 1 START -->
+            <div class="col-lg-3">
+                <!-- logo -->
+                <a class="me-0" href="index.html">
+                    <img class="h-40px" src="images/botlogo.svg" alt="logo">
+                    {{--<img class="dark-mode-item h-40px" src="images/botlogo.svg" alt="logo">--}}
+                </a>
+
+                <p class="mt-4 mb-2" style="color: var(--bs-body-color);">
+                    Ваш надежный агент по закупкам лифтовых запчастей.
+
+                </p>
+            </div>
+            <!-- Widget 1 END -->
+
+            <!-- Widget 2 START -->
+            <div class="col-lg-8 col-xxl-7">
+                <div class="row g-3">
+                    <!-- Link block -->
+                    <div class="col-6 col-md-4">
+                        <h6 class="mb-2 mb-md-4" >Навигация</h6>
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><a class="nav-link-b pt-0" href="about-v1.html">О нас</a></li>
+                            <li class="nav-item"><a class="nav-link-b" href="contact-v1.html">Ассортимент и цены</a></li>
+                            <li class="nav-item"><a class="nav-link-b" href="career.html">Оплата и доставка</a></li>
+                            <li class="nav-item"><a class="nav-link-b" href="career-single.html">Гарантия</a></li>
+                            <li class="nav-item"><a class="nav-link-b" href="contact-v1.html">Документы</a></li>
+                            <li class="nav-item"><a class="nav-link-b" href="sign-in.html">Стать поставщиком</a></li>
+
+                        </ul>
+                    </div>
+
+                    <!-- Link block -->
+                   {{-- <div class="col-6 col-md-4">
+                        <h6 class="mb-2 mb-md-4" style="color: var(--bs-body-color);">Community</h6>
+                        <ul class="nav flex-column">
+                            <li class="nav-item"><a class="nav-link pt-0" href="#">Documents</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Supports <i class="bi bi-box-arrow-up-right small ms-1"></i></a></li>
+                            <li class="nav-item"><a class="nav-link" href="faq.html">Faqs</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Privacy Policy</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-grid.html">News and blogs</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Terms &amp; condition</a></li>
+                        </ul>
+                    </div>--}}
+
+                    <!-- Link block -->
+  {{--                  <div class="col-md-4">
+                        <h6 class="mb-2 mb-md-4">App available on</h6>
+                        <div class="row g-2 mt-2 mb-4 mb-sm-5">
+                            <!-- Google play store button -->
+                            <div class="col-5 col-sm-4 col-md-6">
+                                <a href="#"> <img src="assets/images/elements/google-play.svg" alt=""> </a>
+                            </div>
+                            <!-- App store button -->
+                            <div class="col-5 col-sm-4 col-md-6">
+                                <a href="#"> <img src="assets/images/elements/app-store.svg" alt="app-store"> </a>
+                            </div>
+                        </div>
+
+                        <!-- Social buttons -->
+                        <h6 class="mb-2 mb-md-4" style="color: var(--bs-body-color);">Follow on</h6>
+
+                        <ul class="list-inline mb-0 mt-3">
+                            <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-facebook-f lh-base"></i></a> </li>
+                            <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-instagram lh-base"></i></a> </li>
+                            <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-twitter lh-base"></i></a> </li>
+                            <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-linkedin-in lh-base"></i></a> </li>
+                            <li class="list-inline-item"> <a class="btn btn-xs btn-icon btn-light" href="#"><i class="fab fa-fw fa-youtube lh-base"></i></a> </li>
+                        </ul>
+                    </div>--}}
+
+                </div>
+            </div>
+            <!-- Widget 2 END -->
+        </div>
+
+        <!-- Divider -->
+        <hr class="mt-4 mb-0">
+
+        <!-- Bottom footer -->
+        <div class="d-md-flex justify-content-between align-items-center text-center text-lg-start py-4">
+            <!-- copyright text -->
+            <div class="text-body"> Все права защищены 2024 </div>
+            <!-- copyright links-->
+            <!-- Language selector -->
+            {{--<div class="dropdown dropup text-center text-md-end mt-3 mt-md-0">
+                <a class="dropdown-toggle btn btn-sm btn-light mb-0" href="#" role="button" id="languageSwitcher" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fas fa-globe me-2"></i>Language
+                </a>
+                <ul class="dropdown-menu min-w-auto" aria-labelledby="languageSwitcher">
+                    <li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/uk.svg" alt="">English</a></li>
+                    <li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/gr.svg" alt="">German </a></li>
+                    <li><a class="dropdown-item me-4" href="#"><img class="fa-fw me-2" src="assets/images/flags/sp.svg" alt="">French</a></li>
+                </ul>
+            </div>--}}
+        </div>
+    </div>
+</footer>
+
+
+
+<div class="row mb-3 text-start p-5" style="background: url(/images/botbg.svg) #163649 center  repeat-x ; ">
+    <div class="col-md-3" style="background: url(/images/botlogo.svg)  center no-repeat ;  "><br></div>
+
+    <div class="col-md-4 p-5">
+
+            <p class="text-light w-100">
+            +7 (495) 222-22-22
+        </p>
+
+        <p class="text-light w-100">
+            e-mail: info@partleader.ru
+        </p>
+    </div>
+
+
+    <div class="col-md-4 themed-grid-col">
+        <nav class="navbar  ms-5 border-body" data-bs-theme="navbar-light">
+            <ul class="nav flex-column ms-5" >
+                <!--        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Active</a>
+                        </li>-->
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('about.index')}}">О нас</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('product.index')}}">Ассортимент и цены</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('delivery.index')}}">Оплата и доставка</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('guarantee.index')}}">Гарантия</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('docs.index')}}">Документы</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light fs-6" href="{{route('partner.index')}}">Стать поставщиком</a>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+</div>
+
+
+
+<div class="row justify-content-md-center  m-0 p-0 w-100 " style="background: url(/images/botbg.svg) #163649 center repeat-x ; width: 100%; height: 400px;" >
 
 
     <div class="row d-flex  justify-content-md-center m-0 p-0">
-        <div class="col col-lg  h-100 d-inline-block d-flex align-items-center col-xs-12 col-sm-8 col-md-8">
+        <div class="col col-lg   h-100 d-inline-block d-flex align-items-center col-xs-12 col-sm-8 col-md-8">
             <div class=" align-items-center p-5">
             <p class="text-light w-100">
             +7 (495) 222-22-22
